8718b37775546acb236cb8d3023d2d3adea3a285
U-lll\crunch <crunch@lll.(none)>
2014-07-04 03:08:03 +0200

    added entry 4

---
+++ public/js/4.js
@@ -0,0 +1,17 @@
+var insightID = 4;
+
+var data0or1 = 0;
+
+
+
+$(document).ready(function() {
+    $('#example').dataTable( {
+        "scrollY":        "200px",
+        "scrollCollapse": true,
+        "paging":         false,
+        "bInfo" : false
+    } );
+} );
+
+
+
